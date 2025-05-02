4acafbd31a2027240988338728c29cd150a6c97e
Antreas Lamprou <antreaslamprou12@gmail.com>
2025-05-02 14:51:13 +0300

    Update server logic, and made new js file to handle connection

---
+++ server/connection.js
@@ -0,0 +1,30 @@
+import mysql from 'mysql2';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve('../.env') });
+
+// Build the absolute path to the cert
+const AbsoluteCAPath = path.resolve('../', process.env.CA_CERTIFICATE_PATH);
+const CACertificate = fs.readFileSync(AbsoluteCAPath);
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+      ca: CACertificate
+    }
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err.message);
+    } else {
+      console.log('Connected to the MySQL database.');
+    }
+});
+
+export default db;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,10 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const fs = require('fs');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import db from './connection.js';
 
-const CACertificate = fs.readFileSync(process.env.CA_CERTIFICATE_PATH);
 const app = express();
 app.use(cors()); // Allow React to access this server
 app.use(express.json());
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    ca: CACertificate
-  }
-});
 
 // Offers
 app.get('/', (req, res) => {
